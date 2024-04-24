02fbe68adda2b44b1b9516682b6e48d54de89abe
sitzmaa <103063243+sitzmaa@users.noreply.github.com>
2024-04-24 16:25:42 -0700

    Building Car And Spawner

--- Assets/Scripts/CarScripts/CarMovement.cs
+++ Assets/Scripts/CarScripts/CarMovement.cs
@@ -2,19 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CarMovement : MonoBehaviour
 {
-    //BoxCollider stoppingBox = GameObject.FindGameObjectWithTag("StoppingBox").GetComponent(BoxCollider);
+    public Transform destination;
+    private NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        agent.destination = destination.position;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Stop();
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Go(); // Simple, may require more checks when more objects are added
+    }
+
+    private void Stop()
+    {
         
+    }
+
+    private void Go()
+    {
     }
 }
