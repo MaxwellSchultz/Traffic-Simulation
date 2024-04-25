0c31932e49000b946432de2ba2e137d5c51cd099
BorisWeinfurt <boris@wft.cz>
2024-04-24 22:54:59 -0700

    starting ui

--- Assets/Scripts/Clickables/Source.cs
+++ Assets/Scripts/Clickables/Source.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Source : MonoBehaviour, IsHitReaction
 {   
     public Material hitMaterial; // Reference to the material to be applied when hit
-
     private Material originalMaterial;
     private Renderer objectRenderer;
 
@@ -18,12 +17,10 @@ public class Source : MonoBehaviour, IsHitReaction
     public void ReactToHit()
     {
         objectRenderer.material = hitMaterial;
-        Debug.Log("Source was hit!");
     }
 
     public void UnreactToHit()
     {
         objectRenderer.material = originalMaterial;
-        Debug.Log("Source was unhit");
     }
 }
---
+++ Assets/Scripts/SourceUI.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SourceUI : MonoBehaviour
+{
+    public int rateOfCars;
+    public void OnSliderChange() {
+
+        Debug.Log("slide change");
+    }
+
+}
