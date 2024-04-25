da34cfe1f1be633a4c948db65085e80cb550b7d3
Alex <sitzmaa@wwu.edu>
2024-04-25 10:02:31 -0700

    cars more functional

--- Assets/Scripts/CarScripts/Spawner.cs
+++ Assets/Scripts/CarScripts/Spawner.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject spawnObject;
+    [SerializeField]
+    private float delay = 5.0f;
+    private float timer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = 0;
     }
 
     // Update is called once per frame
@@ -15,9 +21,27 @@ public class Spawner : MonoBehaviour
     {
         
     }
+    
+    void FixedUpdate()
+    {
+        if ( timer > delay) {
+            GameObject newObject = Instantiate(spawnObject);
+            newObject.transform.position = transform.position;
+            newObject.SetActive(true);
+            CarAI carAI = newObject.GetComponent<CarAI>();
+            if (carAI)
+            {
+                GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("Sink");
+                int rand = Random.Range(0, gameObjects.Length);
+                carAI.CustomDestination = gameObjects[rand].transform;
+            }
+            timer = 0;
+        }
+        timer++;
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Car")
+        if (other.gameObject.CompareTag("Car"))
         {
             Destroy(other.gameObject);
         }
