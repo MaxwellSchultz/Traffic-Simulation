d5973e15a38c10a906d13542da4e2f44ae52dae5
BorisWeinfurt <boris@wft.cz>
2024-04-25 13:19:48 -0700

    source ui

--- Assets/Scripts/Spawner/Source.cs
+++ Assets/Scripts/Spawner/Source.cs
@@ -5,22 +5,41 @@ using UnityEngine;
 public class Source : MonoBehaviour, IsHitReaction
 {   
     public Material hitMaterial; // Reference to the material to be applied when hit
+    public GameObject myUIPrefab;
     private Material originalMaterial;
     private Renderer objectRenderer;
+    private GameObject myUI;
+    private GameObject myCanvas;
 
     void Start()
     {
         objectRenderer = GetComponent<Renderer>();
         originalMaterial = objectRenderer.material;
+        myCanvas = GameObject.Find("SourceCanvas");
+        myUI = Instantiate(myUIPrefab);
+        myUI.SetActive(false);
+        myUI.transform.SetParent(myCanvas.transform, false);
     }
 
     public void ReactToHit()
     {
         objectRenderer.material = hitMaterial;
+        myUI.SetActive(true);
     }
 
     public void UnreactToHit()
     {
         objectRenderer.material = originalMaterial;
+        myUI.SetActive(false);
+    }
+
+    public void OnSliderChange() {
+
+        Debug.Log("slide change");
+    }
+
+    public void OnToggleActive() {
+
+        Debug.Log("toggle change");
     }
 }
