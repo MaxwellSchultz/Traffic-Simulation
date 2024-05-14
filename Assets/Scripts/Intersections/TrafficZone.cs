96ac7245710756f58674d7037e70e3c36bcb4554
Alex <sitzmaa@wwu.edu>
2024-05-14 16:45:58 -0700

    sanity

--- Assets/Scripts/Intersections/TrafficZone.cs
+++ Assets/Scripts/Intersections/TrafficZone.cs
@@ -9,16 +9,7 @@ public class TrafficZone : MonoBehaviour
     [SerializeField]
     private int id;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Car"))
