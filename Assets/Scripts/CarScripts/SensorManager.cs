fe79392c7620493ff5ad7730bebf7b0b2decd59f
BorisWeinfurt <boris@wft.cz>
2024-06-02 19:47:20 -0700

    randomly delayed movement

--- Assets/Scripts/CarScripts/SensorManager.cs
+++ Assets/Scripts/CarScripts/SensorManager.cs
@@ -7,12 +7,14 @@ public class SensorManager : MonoBehaviour
     private CarAI carAI;
     public string tagName;
     private bool active;
+    private float startUpDelay = 5;
 
     void Start()
     {
         carAI = gameObject.transform.parent.GetComponent<CarAI>();
         //carAI = gameObject.GetComponent<CarAI>();
         active = true;
+        startUpDelay = Random.Range(0, 5);
     }
 
     private void OnTriggerStay(Collider car)
@@ -39,15 +41,26 @@ public class SensorManager : MonoBehaviour
     {
         if (car.CompareTag("Barrier"))
         {
-            carAI.move = true;
+            SetActiveWithDelay(true, startUpDelay);
         }
         if (car.gameObject.transform.parent != null)
         {
             if (car.gameObject.transform.parent.CompareTag(tagName))
             {
-                carAI.move = true;
+                SetActiveWithDelay(true, startUpDelay);
             }
         }
+    }
+
+    public void SetActiveWithDelay(bool set, float delay)
+    {
+        StartCoroutine(SetActiveAfterDelay(set, delay));
+    }
+
+    private IEnumerator SetActiveAfterDelay(bool set, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        carAI.move = true;
     }
 
     public void Active(bool set)
