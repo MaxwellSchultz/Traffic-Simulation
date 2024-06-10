ef7f5153990f86b70768594a3d62da160e536c96
parash253 <58897920+parash253@users.noreply.github.com>
2024-06-11 02:22:09 +0545

    Tran Camera and Train Button controller implemented

--- Assets/Scripts/TrainScripts/enginescript.cs
+++ Assets/Scripts/TrainScripts/enginescript.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Engine : MonoBehaviour
 {
+    public Camera mainCamera;  // Assign the main camera in the Inspector
+    public Camera followCamera;  // Assign the follow camera in the Inspector
+
     private Rigidbody rb;
 
     [SerializeField] private float power;
@@ -15,10 +18,17 @@ public class Engine : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        mainCamera.gameObject.SetActive(true);
+        followCamera.gameObject.SetActive(false);
     }
 
     private void FixedUpdate()
     {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            SwitchCamera();
+        }
+
         if (Input.GetKey(KeyCode.W))
         {
             Debug.Log("working w");
@@ -41,5 +51,13 @@ public class Engine : MonoBehaviour
         currentSpeed += power * Time.deltaTime;
         Vector3 dir = currentSpeed * transform.forward;
         rb.velocity = dir;
+    }
+    void SwitchCamera()
+    {
+        bool isMainCameraActive = mainCamera.gameObject.activeSelf;
+        
+        // Toggle camera activation
+        mainCamera.gameObject.SetActive(!isMainCameraActive);
+        followCamera.gameObject.SetActive(isMainCameraActive);
     }
 }
